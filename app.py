6a636be10f78f8d50adc36c91591b106d30d52b5
Aleumas <aleumas25@gmail.com>
2021-07-16 14:57:30 +0300

    Update

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def USSDCallback():
                 'request_id':'0',
                 'request':'1. enter first phone number'
             }
-            command1 = 'continue'
+            command1 = 'codfasdfntinue'
             #'continue-1*1'
 
             newData = {
